77cbe584984348cfbfc6f5f25612802c6b3f3f02
anton1113 <anton11131113@gmail.com>
2017-08-01 00:17:21 +0300

    JSF CookBook Chapter 3 - Ajax Example

---
+++ src/main/java/beans/HelloBean.java
@@ -0,0 +1,31 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "helloBean")
+@SessionScoped
+public class HelloBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSayWelcome() {
+
+        if ("".equals(name) || name == null) {
+            return "";
+        } else {
+            return "Ajax message: Welcome " + name;
+        }
+    }
+}
